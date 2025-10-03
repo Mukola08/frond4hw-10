04c849d9750fd4a0887c75f09fc590d266b623ac
Mukola08 <163895904+Mukola08@users.noreply.github.com>
2025-10-03 19:33:46 +0300

    Updates

--- frond4hw-10.0f77c784.js
+++ frond4hw-10.0f77c784.js
@@ -714,6 +714,59 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"2R06K":[function(require,module,exports,__globalThis) {
+// завдання 1
+let count = 0;
+const timers = setInterval(()=>{
+    count += 1;
+    if (count === 5) clearInterval(timers);
+    console.log("hello");
+}, 1000);
+console.log(timers);
+// завдання 2
+const box2EL = document.querySelector(".box2");
+let pos = 0;
+const box2 = setInterval(()=>{
+    pos += 10;
+    box2EL.style.top = pos + "px";
+    box2EL.style.left = pos + "px";
+    if (pos >= 250) clearInterval(box2);
+}, 100);
+//Завдання 3
+const target = document.querySelector(".img"), scoreEl = document.querySelector(".score"), timeEl = document.querySelector(".time");
+let score = 0, time = 10, timer, playing = false;
+function move() {
+    target.style.left = Math.random() * 260 + "px";
+    target.style.top = Math.random() * 260 + "px";
+}
+target.onclick = ()=>{
+    if (playing) {
+        score++;
+        scoreEl.textContent = score;
+        move();
+    }
+};
+document.querySelector(".btn").onclick = ()=>{
+    score = 0;
+    time = 10;
+    playing = true;
+    scoreEl.textContent = 0;
+    timeEl.textContent = 10;
+    move();
+    timer = setInterval(()=>{
+        time--;
+        timeEl.textContent = time;
+        if (time <= 0) {
+            clearInterval(timer);
+            playing = false;
+            alert("\u041E\u0447\u043A\u0438: " + score);
+        }
+    }, 1000);
+};
+//Завдання 4
+document.getElementById("start").addEventListener("click", ()=>{
+    let t = document.getElementById("sec").value;
+    setTimeout(()=>alert("\u0427\u0430\u0441 \u0432\u0438\u0439\u0448\u043E\u0432!"), t * 1000);
+});
 
 },{}]},["7wZbQ","2R06K"], "2R06K", "parcelRequire9ef5", {})
 
